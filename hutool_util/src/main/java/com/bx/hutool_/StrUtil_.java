5e44a7bd176ee3df67d920274f3123870acb7274
lili <2322750326@qq.com>
2025-09-23 13:58:29 +0800

    添加StrUtil.split()分割字符串重载方法

--- hutool_util/src/main/java/com/bx/hutool_/StrUtil_.java
+++ hutool_util/src/main/java/com/bx/hutool_/StrUtil_.java
@@ -11,21 +11,24 @@ public class StrUtil_ {
     //StrUtil 字符串工具类
     public static void main(String[] args) {
         //1、isEmpty()：判断字符串是否为空，不能判断全是空白串的情况
-        //2、isBlank()：判断字符串是否为空或是否是空白串
+        //2、isBlank()：判断字符串“是否为空”或“是否为空白串”
         String s = new String();
         System.out.println("StrUtil.isEmpty(s) = " + StrUtil.isEmpty(s));
         System.out.println("StrUtil.isBlank(s) = " + StrUtil.isBlank(s));
-        s="     ";
-        System.out.println("StrUtil.isEmpty(s) = " + StrUtil.isEmpty(s));
-        System.out.println("StrUtil.isBlank(s) = " + StrUtil.isBlank(s));
 
-        //5、trim()：去除字符串的收尾空白
+        String blankS="     ";
+        System.out.println("StrUtil.isEmpty(blankS) = " + StrUtil.isEmpty(blankS));
+        System.out.println("StrUtil.isBlank(blankS) = " + StrUtil.isBlank(blankS));
+
+        //5、trim()：去除字符串的首尾空白
         String s1 = new String("    jfjd    jkfj    ");
         System.out.println("StrUtil.trim(s1) = " + StrUtil.trim(s1));
 
         //6、分割字符串
-        String s2 = new String("a,b,c");
-        System.out.println("(StrUtil.split(s2, ',')) = " + (StrUtil.split(s2, ',')));
+        String s2 = new String("a,b,c,d");
+        StrUtil.split(s2, ',').forEach(System.out::println);//StrUtil.split(s2, ',')按,分s2字符串
+        System.out.println("==================================");
+        StrUtil.split(s2, ',', 3).forEach(System.out::println);//StrUtil.split(s2, ',', 3)按,分割s2最多分成3个
 
     }
 }
