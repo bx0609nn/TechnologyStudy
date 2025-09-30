7f9b68687fc77a2bffb159c124d3c491f6ecbbfd
lili <2322750326@qq.com>
2025-09-30 16:26:06 +0800

    新增fastjson和hutool两个工具类对字符串、JSONObject、JSONArray、Java对象之间的转换

--- fastjson/src/main/java/com/bx/json/JsonTo.java
+++ fastjson/src/main/java/com/bx/json/JsonTo.java
@@ -3,6 +3,7 @@ package com.bx.json;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.bx.entity.Student;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,42 +16,59 @@ import java.util.Map;
  */
 public class JsonTo {
     public static void main(String[] args) {
-        //将json对象解析为JsonObject
-        String jsonObj = "{\"id\":1,\"name\":\"zhangsan\",\"age\":25}";
+        //1、将json字符串解析为JSONObject JSON.parseObject()--------------------------------------------------------------
+        String jsonObjStr = "{\n" +
+                "    \"student\": {\n" +
+                "        \"id\": 1,\n" +
+                "        \"name\": \"zhangsan\",\n" +
+                "        \"age\": 15\n" +
+                "    },\n" +
+                "    \"teacher\": {\n" +
+                "        \"id\": 1,\n" +
+                "        \"name\": \"lisi\",\n" +
+                "        \"age\": 36\n" +
+                "    }\n" +
+                "}";
 
-        JSONObject object = JSON.parseObject(jsonObj);
-        System.out.println("object = " + object);
-        //获取简单属性
-        int id = object.getIntValue("id");//int类型的值
-        System.out.println("id = " + id);
-        String name = object.getString("name");//字符串类型的值
-        System.out.println("name = " + name);
+        JSONObject jsonObject = JSON.parseObject(jsonObjStr);
 
-        //将json解析为JSONArray
+        //1.1JSONObject获取子JSONObject jsonObject.getJSONObject()
+        JSONObject student = jsonObject.getJSONObject("student");
+        //1.2JSONObject获取简单属性
+        int id = student.getIntValue("id");//int类型的值，不存在该键则返回0
+        String name = student.getString("name");//String类型的值，若不存在该键则返回null
+        jsonObject.getString("student");//也可以将嵌套的对象获取为String，然后可以直接解析为Java对象
+
+
+
+        //2、将json字符串解析为JSONArray JSON.parseArray()----------------------------------------------------------------
         String jsonArrayStr = "[{\"name\":\"张三\"}, {\"name\":\"李四\"}]";
         JSONArray jsonArray = JSON.parseArray(jsonArrayStr);
-        //获取json数组中的第一个对象
+        //2.1 获取JSONArray中的第一个对象 jsonArray.getJSONObject(0)
         System.out.println("jsonArray.getJSONObject(0) = " + jsonArray.getJSONObject(0));
 
-        //将Java对象序列化为json
-        HashMap hashMap = new HashMap<>();
-        hashMap.put("age",22);
-        hashMap.put("name","张三");
 
-        String jsonString = JSON.toJSONString(hashMap);
-        System.out.println("jsonString = " + jsonString);
 
-        //将JSON解析为Java对象
-        Map map = JSON.parseObject(jsonString, Map.class);
-        System.out.println("map = " + map);
+        //3、将json字符串解析为Java对象 JSON.parseObject();---------------------------------------------------------------
+        Student studentObj = JSON.parseObject(String.valueOf(student), Student.class);
+        System.out.println("studentObj = " + studentObj);
+
+        jsonObject.getString("student");//也可以将嵌套的对象获取为String，然后可以直接解析为Java对象
+        JSON.parseObject(jsonObject.getString("student"), Student.class);
+
+
+        //4、将Java对象序列化为json字符串 JSON.toJSONString()-------------------------------------------------------------
+        Student student1 = new Student(2l, "Jerry", 16, "男");
+
+        String javaToJsonString = JSON.toJSONString(student1);
+        System.out.println("javaToJsonString = " + javaToJsonString);
+
+
 
         //总结：
-        //转JSON对象 JSON.parseObject();
-        //转JSON数组 JSON.parseArray();
-        //转JSON字符串 JSON.toJSONString();
-        //转Java对象 JSON.parseObject(jsonString, Map.class);
-
-
-
+        //字符串转JSONObject JSON.parseObject();
+        //字符串转JSONArray JSON.parseArray();
+        //字符串转Java对象 JSON.parseObject(jsonString, Map.class);
+        //Java对象转json字符串 JSON.toJSONString();
     }
 }
--- hutool_util/src/main/java/com/bx/hutool_/JSONUtil_.java
+++ hutool_util/src/main/java/com/bx/hutool_/JSONUtil_.java
@@ -1,5 +1,6 @@
 package com.bx.hutool_;
 
+import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.bx.entity.Student;
@@ -14,32 +15,52 @@ import java.util.HashMap;
  */
 public class JSONUtil_ {
     public static void main(String[] args) {
-        //1、将JSON字符串转为Java对象 JSONUtil.toBean()
-        String jsonStr = "{\"id\":12,\"name\":\"Tom\",\"age\":25}";
-        Student student = JSONUtil.toBean(jsonStr, Student.class);
-        System.out.println("student = " + student.toString());
-
-        //2、Java对象转JSON字符串 JSONUtil.toJsonStr()
-        Student student1 = new Student(1l,"Tom", 25,"男");
-        String studentJsonStr = JSONUtil.toJsonStr(student1);
-        System.out.println("studentJsonStr = " + studentJsonStr);
-
-        //3、JSON字符串转JSON对象 JSONUtil.parseObj()
-        String jsonStr2 = "{\"data\": {\n" +
+        //1、将json字符串转JSONObject JSONUtil.parseObj()----------------------------------------------------------------
+        String jsonObjStr = "{\"data\": {\n" +
                 "  \"name\": \"Tom\",\n" +
                 "  \"age\": 25\n" +
                 "}}";
-        JSONObject jsonObject = JSONUtil.parseObj(jsonStr2);
-        System.out.println("jsonObject = " + jsonObject);
-        String data = jsonObject.getStr("data");
-        System.out.println("jsonObject.getObj(\"data\") = " + data);
-        HashMap<String,String> hashMap = JSONUtil.toBean(data, HashMap.class);
-        String name = hashMap.get("name");
-        System.out.println("name = " + name);
+        JSONObject jsonObject = JSONUtil.parseObj(jsonObjStr);
 
-        //总结：转Java对象JSONUtil.toBean()
-        //     转JSON字符串JSONUtil.toJsonStr()
-        //     转JSON对象JSONUtil.parseObj()
+        //1.1 JSONObject获取子JSONObject
+        JSONObject data = jsonObject.getJSONObject("data");
+        //1.2 JSONObject获取简单属性
+        String name = data.getStr("name");//获取String类型值，若不存在改键则返回null
+        Integer age = data.getInt("age");//获取int类型值，若不存在改键则返回null
+
+
+
+        //2、将json字符串转为JSONArray JSONUtil.parseArray()--------------------------------------------------------------
+        String jsonArrayStr = "[{\"name\":\"张三\"}, {\"name\":\"李四\"}]";
+        JSONArray jsonArray = JSONUtil.parseArray(jsonArrayStr);
+        //JSONArray获取第一个对象
+        jsonArray.get(0,Student.class);
+
+
+
+        //3、json字符串/JSONObject解析为Java对象 JSONUtil.toBean()--------------------------------------------------------
+        //3.1 json字符串-->Java对象
+        String jsonStr = "{\"id\":12,\"name\":\"Tom\",\"age\":25}";
+        Student student = JSONUtil.toBean(jsonStr, Student.class);
+
+        //3.2 JSONObject-->Java对象
+        HashMap<String,String> hashMap = JSONUtil.toBean(data, HashMap.class);
+
+
+
+        //4、Java对象转json字符串 JSONUtil.toJsonStr()--------------------------------------------------------------------
+        Student student1 = new Student(1l,"Tom", 25,"男");
+
+        String studentJsonStr = JSONUtil.toJsonStr(student1);
+        System.out.println("studentJsonStr = " + studentJsonStr);
+
+
+
+        //总结：
+        //1.字符串转JSONObject JSONUtil.parseObj()
+        //2.字符串转JSONArray JSONUtil.parseArray();
+        //3.字符串转Java对象 JSONUtil.toBean()
+        //4.Java对象转json字符串 JSONUtil.toJsonStr()
 
         String jsonStr3 = "{\"data\":{\n" +
                 "  \"name\": \"Tom\",\n" +
@@ -49,10 +70,10 @@ public class JSONUtil_ {
         System.out.println("jsonObject1.getObj(\"data\") = " + jsonObject1.getObj("data"));//JSON格式对象
         System.out.println("jsonObject1.getBean(\"data\",Student.class) = " + jsonObject1.getBean("data", Student.class));//Student
 
-        //4、JSONObject获取数据
-        //1.根据key获取嵌套JSON对象 getObj("data")，返回的是一个JSON对象{"name":"Tom","age":25}
+        //JSONObject获取数据
+        //1.根据key获取嵌套JSON对象 getJSONObject("data") 或 getObj("data")，返回的是一个JSON对象{"name":"Tom","age":25}
         //2.根据key获取字符串类型数据 getStr("name")
-        //3.根据key获取double类型数据 getInt("age")
+        //3.根据key获取int类型数据 getInt("age")
         //4.根据key获取对象 getBean("data", Student.class))，内部实现会先将 key 对应的值解析为一个 JSONObject，然后调用 toBean 方法转换为 Java 对象。
 
     }
