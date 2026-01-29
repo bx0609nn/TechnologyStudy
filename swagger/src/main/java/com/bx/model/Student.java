542d1fbcf9877635b92aa0e4e599f67fd2bf8bf5
lili <2322750326@qq.com>
2026-01-29 15:31:46 +0800

    日志配置从使用Logback修改为使用Log4j2

--- swagger/src/main/java/com/bx/model/Student.java
+++ swagger/src/main/java/com/bx/model/Student.java
@@ -11,12 +11,48 @@ import lombok.NoArgsConstructor;
  * @description
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Student {
     private Long id;
     private String name;
     private Integer age;
-
     private String sex;
+
+    public Student(Long id, String name, Integer age, String sex) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
